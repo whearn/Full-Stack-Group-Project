fb8604300465f81e9ee74308736942c39c2a3a88
RayAdams <cramerrachael@gmail.com>
2017-07-17 12:21:44 -0400

    added index.js

---
+++ server/index.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var prerender = require('prerender-node');
+var api = require('./api');
+var routing = require('./middleware/routing.mw');
+
+var clientPath = path.join(__dirname, '../client');
+
+//add group prerender token
+prerender.set('prerenderToken', process.env.PRERENDER_TOKEN);
+
+var app = express();
+app.use(prerender);
+app.use(express.static(clientPath));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use('/api', api);
+
+app.get('*', routing.stateRouting);
+app.listen(process.env.PORT || 3000);
--- server/services/stripe.svc.js
+++ server/services/stripe.svc.js
@@ -1,5 +1,6 @@
 var stripe = require('stripe')(process.env.STRIPE_TEST_SECRET_KEY);
 // how to set to process.env.STRIPE_TEST_SECRET_KEY
+
 //add group stripe key
 exports.charge = function(token, amt) {
     return stripe.charges.create({

be9aec01ae3f9456bc125a97492d057820d6fdb5
RayAdams <cramerrachael@gmail.com>
2017-07-20 13:57:54 -0400

    checkout done

--- server/controllers/checkout.ctrl.js
+++ server/controllers/checkout.ctrl.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var stripeSvc = require('../services/stripe.svc');
 var mailService = require('../services/email.svc');
+var procedures = require('../procedures/purchase.proc');
 
 var router = express.Router();
 
@@ -8,12 +9,14 @@ router.post('/', function(req, res) {
     stripeSvc.charge(req.body.token, req.body.amount)
     .then(function(response) {
         console.log(response);
-        res.sendStatus(201);
-    }).catch(function(err) {
+        return procedures.create(req.body.amount, response.id);
+    }).then(function(response) {
+        res.status(201).send(response);
+    })
+    .catch(function(err) {
         console.log(err);
         res.sendStatus(500);
     });
-
     // let content = `<h3>Thank you for shopping with Covalence ${req.body.name}</h3>
     //                 <h5>From:${req.body.products}</h5>
     //                 <p>${req.body.total}</p>`;

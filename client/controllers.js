ecac36916a4f2f702b10230996b32485a788dca4
RayAdams <cramerrachael@gmail.com>
2017-07-19 22:46:06 -0400

    worked on checkout

--- client/controllers.js
+++ client/controllers.js
@@ -5,6 +5,15 @@ angular.module('storeApp.controllers', [])
         url: $location.url(),
         description: 'Welcome to the Covalence Store'
     })
+
+}])
+.controller('NavController', ['$scope', '$location', function($scope, $location) {
+    if(localStorage.items === undefined) 
+        localStorage.items = angular.toJson([]);
+    $scope.cartTotal = angular.fromJson(localStorage.items).length;
+
+    //look up rootscope.prodcast to refresh badge counter
+    //create service for navbar if dont want to use above
 }])
 .controller('ApparelController', ['$scope', 'Apparel', 'SEOService', '$location', function($scope, Apparel, SEOService, $location) {
     $scope.apparels = Apparel.query();
@@ -26,6 +35,17 @@ angular.module('storeApp.controllers', [])
 }])
 .controller('ProductController', ['$scope', 'Product', 'SEOService', '$location', '$routeParams', function($scope, Product, SEOService, $location, $routeParams) {
     $scope.product = Product.get({id: $routeParams.id});
+
+    if(localStorage.items === undefined) 
+        localStorage.items = JSON.stringify([]);
+
+    $scope.addItem = function() {
+        alert('Item added to Cart!');
+        let cachedItems = JSON.parse(localStorage.items);// "[[title, description, image, price],[title, description, image, price]]"
+        cachedItems.push($scope.product);// [[title, description, image, price],[title, description, image, price], ...]
+        localStorage.items = JSON.stringify(cachedItems);// "[[title, description, image, price],[title, description, image, price], ...] "
+        //localStorage.setItem('product', JSON.stringfy($scope.product)); <-previous way
+    }
 
     SEOService.setSEO({
             title: $scope.product.title,
@@ -54,6 +74,45 @@ angular.module('storeApp.controllers', [])
 }])
 .controller('CheckoutController', ['$scope', '$location', 'Checkout', 'SEOService', function($scope, $location, Checkout, SEOService) {
     //stripe global variable created in index.html
+
+    if(localStorage.items === undefined) 
+        localStorage.items = angular.toJson([]);
+
+    $scope.cart = angular.fromJson(localStorage.items);  
+
+    let total = 0;
+    for (let i = 0; i < $scope.cart.length; i++) {
+        total += $scope.cart[i].price;
+    }
+    $scope.total = total
+
+    $scope.removeItem = function(product) {
+        let index = $scope.cart.indexOf(product)
+        
+        if (index > -1) {
+            $scope.cart.splice(index, 1);
+        }
+
+        localStorage.items = angular.toJson($scope.cart);
+        $scope.total -= product.price;
+    }
+
+    let discountCode10 = 'ThunderCats';
+    let discountCode20 = 'Garlic.io';
+    let discountsToBeApplied = 1;
+
+    $scope.applyDiscount = function() {
+        if($scope.discountValue === discountCode10 && discountsToBeApplied >= 1) {
+            $scope.total *= .9;
+            discountsToBeApplied = 0;
+        }
+
+        if($scope.discountValue === discountCode20 && discountsToBeApplied >= 1) {
+            $scope.total *= .8;
+            discountsToBeApplied = 0;
+        }
+    }
+
     var elements = stripe.elements();
     var card = elements.create('card');
     card.mount('#card-field');
@@ -88,6 +147,7 @@ angular.module('storeApp.controllers', [])
             }
         });
     }
+
 
     SEOService.setSEO({
         title: 'Covalence Store - Checkout',
--- server/procedures/purchase.proc.js
+++ server/procedures/purchase.proc.js
@@ -1,12 +1,6 @@
 var db = require('../config/db');
 
-//match to DB stored procedures
-exports.all = function() {
-    return db.rows('GetChart');
-}
-exports.destroy = function(id) {
-    return db.empty('DeleteItem', [id]);
-}
+//match to DB stored procedure for new purchase
 exports.create = function(productid, purchaseid) {
     return db.row('InsertPurchase', [productid, purchaseid])
 }
